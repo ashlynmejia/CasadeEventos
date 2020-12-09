a327130ed7bf8a23d86fa0004dbc4e668951745a
ashlynmejia <56077273+ashlynmejia@users.noreply.github.com>
2020-12-09 15:08:51 -0500

    version1.2

--- base_de_datos1.sql
+++ base_de_datos1.sql
@@ -2,12 +2,12 @@ create table casa_de_eventos(
     nombre varchar(25) primary key,
     reservacion varchar(20),
     evento varchar(36),
-    tiempo_contrato varchar(10),
+    tiempo_contrato integer,
     cedula integer
 );
 
 create table decoracion(
-    id varchar(15) primary key,
+    id integer primary key auto_increment,
     gastronomia varchar(15),
     topico varchar(40),
     color_principal varchar(5)
@@ -17,40 +17,40 @@ create table personal(
     nombre varchar(25) primary key,
     cedula integer,
     especialidad varchar(60),
-    sueldo varchar(10)
+    sueldo integer
 );
 
 create table contrato(
     tiempo_contrato varchar(15) primary key,
     especialidad varchar(18),
-    sueldo varchar(10)
+    sueldo integer
 );
 
 create table evento(
     nombre varchar(30),
-    especialidad varchar(36),
+    evento varchar(36),
     fecha_hora date,
 
     primary key (nombre,
-    especialidad,fecha_hora)
+    evento, fecha_hora)
 );
 
 alter table casa_de_eventos
 add foreign key (cedula) 
-references decoracion (id);
+references contrato (tiempo_contrato);
 
 alter table decoracion
 add foreign key (color_principal)
-references evento (evento);
+references casa_de_eventos (nombre);
 
 alter table personal
 add foreign key (sueldo)
+references eventos (nombre);
+
+alter table eventos
+add foreign key (nombre)
 references contrato (tiempo_contrato);
 
-alter table evento
-add foreign key (nombre)
-references decoracion (topico);
-
-alter table evento
-add foreign key (nombre)
-references nombre (personal);
+alter table eventos
+add foreign key (evento)
+references decoracion (id); 
